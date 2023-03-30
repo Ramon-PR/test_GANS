645b30fa2066da581377be3456766a46293257eb
Ramon Pozuelo Ruiz <pozueloruizramon@gmail.com>
2023-03-30 13:14:33 +0200

    Update test_CNN_batchnorm.py

--- test_CNN_batchnorm.py
+++ test_CNN_batchnorm.py
@@ -4,13 +4,14 @@ Created on Wed Feb 22 18:20:20 2023
 
 @author: Ramón Pozuelo
 """
+
 from pathlib import Path
 # import sys
-from module_CNN_ZEA import load_DB_ZEA, down_samp_transf, ZeaDataset, fit
+from module_CNN_ZEA import load_DB_ZEA
 
-# path_script = Path(r"C:\Users\keris\Desktop\Postdoc")
+path_script = Path(r"C:\Users\keris\Desktop\Postdoc")
 # path_script = Path(r"/scratch/ramonpr/3NoiseModelling")
-path_script = Path(r"C:\Users\keris\Desktop")
+# path_script = Path(r"C:\Users\keris\Desktop")
 
 folder_Data = "DataBase_Zea"
 files = ["BalderRIR.mat", "FrejaRIR.mat", "MuninRIR.mat"]
@@ -26,13 +27,14 @@ Y, _ = load_DB_ZEA(path, nx, nt)
 # %% Definir Dataset
 import torch
 import torchvision
+from module_CNN_ZEA import down_samp_transf, ZeaDataset
+
 RIR_mean, RIR_min, RIR_max = Y.mean(), Y.min(), Y.max()
 RIR_amp = RIR_max - RIR_min
 std = max(RIR_max, abs(RIR_min))
 
 transformaciones = torchvision.transforms.Compose([
-        torchvision.transforms.RandomVerticalFlip(p=0.75),
-        torchvision.transforms.RandomHorizontalFlip(p=0.75),
+        torchvision.transforms.RandomHorizontalFlip(p=0.5),
         torchvision.transforms.Normalize((0.0), (std)) # ( img - mean)/std
     ])
 
@@ -55,30 +57,23 @@ dataloader = {
 
 # %% Definir NN
 
-# resnet = torchvision.models.resnet18(pretrained=True)
-# num_classes = nt*nx
-# resnet.fc = torch.nn.Linear(resnet.fc.in_features, num_classes)
-# resnet
 from NNmodel_classes import CNN_batchnorm
 import timeit
 
-# nepochs=500
-
 n_channels, Hin, Win, Hout, Wout = 1, 32, 32, 32, 32
-# model = CNN(n_channels, Hin, Win, Hout, Wout)
 model = CNN_batchnorm(n_channels, Hin, Win, Hout, Wout)
 
 from NNmodel_classes import count_parameters
-# print(count_parameters(model))
 print("\nNumber of trainable parameters: %i " % (count_parameters(model)))
 
 
-# %%
+# %% Train the model
+from module_CNN_ZEA import load_DB_ZEA, down_samp_transf, fit
 
-nepochs=500
+nepochs=10
 
 # optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=0)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=0)
 
 # multiplica el lr por 0.1 cada 10 epochs
 # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 10, 0.1)
@@ -94,6 +89,7 @@ print("\nThe training time is %f sec" % (tStop - tStart))
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 fig = plt.figure(dpi=200, figsize=(3,3))
 ax = plt.subplot(211)
 pd.DataFrame(hist).plot(x='epoch', y=['loss', 'val_loss'], grid=True, ax=ax)
@@ -102,9 +98,11 @@ pd.DataFrame(hist).plot(x='epoch', y=['lr'], grid=True, ax=ax)
 plt.show()
 
 
-# %% PLOT EXAMPLE IMAGE
+# %% PLOT EXAMPLE IMAGE from validation
 
-idx=0
+fig = plt.figure(dpi=200, figsize=(3,3))
+
+idx=200
 image_in, label_in = dataset['val'][idx]
 # Add batch dimension before inputting into the model
 image_in = image_in[None,:,:,:]
@@ -126,4 +124,33 @@ plt.subplot(2,1,i)
 plt.tight_layout()
 plt.title("Output image from the trained model")
 plt.imshow(image_out.squeeze(), cmap="gray")
+
+# %% PLOT EXAMPLE IMAGE from training
+
+fig = plt.figure(dpi=200, figsize=(3,3))
+
+idx=0
+image_in, label_in = dataset['train'][idx]
+# Add batch dimension before inputting into the model
+image_in = image_in[None,:,:,:]
+model.eval()
+image_out = model(image_in)
+image_out = image_out.reshape(Hout, Wout)
+
+image_out = image_out.cpu().detach().numpy()
+image_in = image_in.cpu().detach().numpy()
+
+i=1
+plt.subplot(2,1,i)
+plt.tight_layout()
+plt.title("Input image")
+plt.imshow(image_in.squeeze(), cmap="gray")
+
+i=2
+plt.subplot(2,1,i)
+plt.tight_layout()
+plt.title("Output image from the trained model")
+plt.imshow(image_out.squeeze(), cmap="gray")
+
+
 
