97eeed7627f25e69a3ea3330752ac12013f9dfa7
Ramon Pozuelo Ruiz <pozueloruizramon@gmail.com>
2023-03-30 13:06:21 +0200

    Only do horizontal flip

--- test_CNN_2branch_min_max_pool.py
+++ test_CNN_2branch_min_max_pool.py
@@ -12,7 +12,7 @@ Epoch 400/400 loss 0.00005  val_loss 0.00071
 
 from pathlib import Path
 # import sys
-from module_CNN_ZEA import load_DB_ZEA, down_samp_transf, ZeaDataset, fit
+from module_CNN_ZEA import load_DB_ZEA
 
 path_script = Path(r"C:\Users\keris\Desktop\Postdoc")
 # path_script = Path(r"/scratch/ramonpr/3NoiseModelling")
@@ -32,13 +32,14 @@ Y, _ = load_DB_ZEA(path, nx, nt)
 # %% Definir Dataset
 import torch
 import torchvision
+from module_CNN_ZEA import down_samp_transf, ZeaDataset
+
 RIR_mean, RIR_min, RIR_max = Y.mean(), Y.min(), Y.max()
 RIR_amp = RIR_max - RIR_min
 std = max(RIR_max, abs(RIR_min))
 
 transformaciones = torchvision.transforms.Compose([
-        torchvision.transforms.RandomVerticalFlip(p=0.75),
-        torchvision.transforms.RandomHorizontalFlip(p=0.75),
+        torchvision.transforms.RandomHorizontalFlip(p=0.5),
         torchvision.transforms.Normalize((0.0), (std)) # ( img - mean)/std
     ])
 
@@ -72,6 +73,7 @@ print("\nNumber of trainable parameters: %i " % (count_parameters(model)))
 
 
 # %% Train the model
+from module_CNN_ZEA import load_DB_ZEA, down_samp_transf, fit
 
 nepochs=10
 
@@ -101,7 +103,9 @@ pd.DataFrame(hist).plot(x='epoch', y=['lr'], grid=True, ax=ax)
 plt.show()
 
 
-# %% PLOT EXAMPLE IMAGE
+# %% PLOT EXAMPLE IMAGE from validation
+
+fig = plt.figure(dpi=200, figsize=(3,3))
 
 idx=200
 image_in, label_in = dataset['val'][idx]
@@ -126,3 +130,29 @@ plt.tight_layout()
 plt.title("Output image from the trained model")
 plt.imshow(image_out.squeeze(), cmap="gray")
 
+# %% PLOT EXAMPLE IMAGE from training
+
+fig = plt.figure(dpi=200, figsize=(3,3))
+
+idx=0
+image_in, label_in = dataset['train'][idx]
+# Add batch dimension before inputting into the model
+image_in = image_in[None,:,:,:]
+model.eval()
+image_out = model(image_in)
+image_out = image_out.reshape(Hout, Wout)
+
+image_out = image_out.cpu().detach().numpy()
+image_in = image_in.cpu().detach().numpy()
+
+i=1
+plt.subplot(2,1,i)
+plt.tight_layout()
+plt.title("Input image")
+plt.imshow(image_in.squeeze(), cmap="gray")
+
+i=2
+plt.subplot(2,1,i)
+plt.tight_layout()
+plt.title("Output image from the trained model")
+plt.imshow(image_out.squeeze(), cmap="gray")
